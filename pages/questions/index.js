7d4b1e21cc9f7f3671645bc8c75e7b608bc2c413
kumarabhishekbansal <kumarabhishekbansal>
2024-03-27 12:25:06 +0530

    setting up a pagination

--- pages/questions/index.js
+++ pages/questions/index.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import { useState, useEffect } from "react";
 import Card from "../../components/Card";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import Pagination from "../../components/Pagination";
 const QuestionsContainer = styled.div`
   display: flex;
   justify-content: space-between;
@@ -15,42 +17,52 @@ const CardLink = styled.a`
 function Questions() {
   const [loading, setLoading] = useState(false);
   const [questions, setQuestions] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
+  const router = useRouter();
+  console.log(router);
+  const { page } = router.query;
   useEffect(() => {
     async function fetchData() {
       const data = await fetch(
-        "https://api.stackexchange.com/2.2/questions?order=desc&sort=hot&tagged=reactjs&site=stackoverflow"
+        `https://api.stackexchange.com/2.2/questions?${
+          page ? `page=${parseInt(page)}&` : ""
+        }order=desc&sort=hot&tagged=reactjs&site=stackoverflow`
       );
       const result = await data.json();
       if (result) {
         setQuestions(result.items);
+        setHasMore(result.has_more);
         setLoading(false);
       }
     }
     fetchData();
-  }, []);
+  }, [page]);
   return (
     <QuestionsContainer>
       <h2>Questions</h2>
       {loading ? (
         <span>Loading...</span>
       ) : (
-        <div>
-          {questions.map((question) => (
-            <Link
-              key={question.question_id}
-              href={`/questions/${question.question_id}`}
-              passHref
-            >
-              <CardLink>
-                <Card
-                  title={question.title}
-                  views={question.view_count}
-                  answers={question.answer_count}
-                />
-              </CardLink>
-            </Link>
-          ))}
-        </div>
+        <>
+          <div>
+            {questions.map((question) => (
+              <Link
+                key={question.question_id}
+                href={`/questions/${question.question_id}`}
+                passHref
+              >
+                <CardLink>
+                  <Card
+                    title={question.title}
+                    views={question.view_count}
+                    answers={question.answer_count}
+                  />
+                </CardLink>
+              </Link>
+            ))}
+          </div>
+          <Pagination currentPage={parseInt(page) || 1} hasMore={hasMore} />
+        </>
       )}
     </QuestionsContainer>
   );
